02259c3aecb7ade966d942c6299848d1871dd899
govinduchaitanyakrishna <govinduchaitanya3@gmail.com>
2022-12-10 16:53:07 +0530

    code

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT||9905;
 
-app.use(express.static(path.join(__dirname,'build')));
+app.use(express.static(path.join(__dirname,'docs')));
 
 app.get('/',(req,res) => {
-    res.send('build/index.html')
+    res.send('docs/index.html')
 });
 
 app.listen(port);
